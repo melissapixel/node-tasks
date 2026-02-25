ca9fbc16f62d878697a7889894a83c2e1bd82e24
Mel <melissapixell@gmail.com>
2026-02-25 11:51:05 +0300

    add simple route (GET)
    - add simple endpoint `\`, `\about`
    - add processing 404

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const http = require('http');
 // 2. Создаём сервер
 const server = http.createServer((req, res) => {
   
-  // 3. Обработка запроса
-  console.log('Получен запрос:', req.method, req.url);
-  
-  // 4. Отправка ответа
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Привет!');
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Main</h1>');
+  }
+
+  if (req.url === '/about') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>About</h1>');
+  }
+
+  // (404)
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Страница не найдена');
+  }
   
 });
 
