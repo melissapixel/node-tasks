4eb2f1c62376fdef4d098a1b5accd75127e4b929
Mel <melissapixell@gmail.com>
2026-02-25 11:58:15 +0300

    add log
    - fix bag with if-else
    - add log in the START route
    - add english content in 404

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const http = require('http');
 // 2. Создаём сервер
 const server = http.createServer((req, res) => {
   
+  // Логируем каждый запрос
+  console.log('Получен запрос:', req.method, req.url);
+  
   if (req.url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Main</h1>');
   }
 
-  if (req.url === '/about') {
+  else if (req.url === '/about') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>About</h1>');
@@ -20,7 +23,7 @@ const server = http.createServer((req, res) => {
   else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Страница не найдена');
+    res.end('Page 404');
   }
   
 });
